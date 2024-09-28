b4a6c4dab74ac4884862798301c8cc338d842251
Lepilkin Aleksandr <heist2000@gmail.com>
2024-09-29 00:27:15 +0300

    add class My_request

--- app/helper/config.py
+++ app/helper/config.py
@@ -7,6 +7,37 @@ URL = os.getenv("URL")
 LOGIN = os.getenv("LOGIN")
 PASSWORD = os.getenv("PASSWORD")
 
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Cookie": "_ym_uid=1724226012457725689; _ym_d=1724226012; gc_visitor_59148=%7B%22id%22%3A6248834160%2C%22sfix%22%3A1%7D; dd_bdfhyr=55f523ba3ce1344d40e5e8efe4683d06; gdpr=0; _ym_isad=1; gc_visit_59148=%7B%22id%22%3A11260577637%2C%22sid%22%3A6502167147%7D; _ym_visorc=w; PHPSESSID5=3de767b7cc6fce50cacb3086bdd88c2b; _csrf=Lm9pU0leG0e0Tg3Qmnh_BsKwo0UcrQVB; gc_counter_59148=%7B%22id%22%3A6502167147%2C%22last_activity%22%3A%222024-09-28%2023%3A48%3A02%22%2C%22user_id%22%3A400267093%2C%22utm_id%22%3A1256%2C%22partner_code_id%22%3Anull%2C%22ad_offer_id%22%3Anull%2C%22fuid%22%3Anull%2C%22fpid%22%3Anull%2C%22city_id%22%3Anull%7D",
+    "Referer": "https://healthcreation.ru/cms/system/login?required=true",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+}
+COOKIES = {
+    "_ym_uid": "1724226012457725689",
+    "_ym_d": "1724226012",
+    "gc_visitor_59148": "%7B%22id%22%3A6248834160%2C%22sfix%22%3A1%7D",
+    "dd_bdfhyr": "55f523ba3ce1344d40e5e8efe4683d06",
+    "gdpr": "0",
+    "_ym_isad": "1",
+    "gc_visit_59148": "%7B%22id%22%3A11260577637%2C%22sid%22%3A6502167147%7D",
+    "_ym_visorc": "w",
+    "PHPSESSID5": "3de767b7cc6fce50cacb3086bdd88c2b",
+    "_csrf": "Lm9pU0leG0e0Tg3Qmnh_BsKwo0UcrQVB",
+    "gc_counter_59148": "%7B%22id%22%3A6502167147%2C%22last_activity%22%3A%222024-09-28%2023%3A48%3A02%22%2C%22user_id%22%3A400267093%2C%22utm_id%22%3A1256%2C%22partner_code_id%22%3Anull%2C%22ad_offer_id%22%3Anull%2C%22fuid%22%3Anull%2C%22fpid%22%3Anull%2C%22city_id%22%3Anull%7D",
+}
 
 if __name__ == "__main__":
-    [print(f"{k} = {v}") for k, v in locals().items() if k.isupper()]
+    print("\nConstants:")
+    [print(f"\t{k} = {v}") for k, v in locals().items() if k.isupper()]
