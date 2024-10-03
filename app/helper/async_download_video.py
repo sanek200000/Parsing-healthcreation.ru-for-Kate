4fb38234d3fbd514a1e955111137ff9bd39c4673
Lepilkin Aleksandr <heist2000@gmail.com>
2024-10-03 10:28:49 +0300

    add progress ber

--- app/helper/async_download_video.py
+++ app/helper/async_download_video.py
@@ -3,11 +3,13 @@ import m3u8
 import aiohttp
 import asyncio
 import shutil
-from helper.logs import logger
+from progress.bar import IncrementalBar as IB
+
+from helper.logging_app import logger
 
 
 class DownloadHLSAsync:
-    def __init__(self, players_list_url: str, path: str) -> None:
+    def __init__(self, players_list_url: str, path: str):
         """When initializing the class, we get a link to the streaming
         video playlist in the format m3u8, write it to the variable self.playlist_url.
         We take the last link from the list, it has the highest resolution.
@@ -33,7 +35,7 @@ class DownloadHLSAsync:
             logger.error(f"Error in class {__class__}: {ex}")
 
     @property
-    def initialized(self):
+    def is_init(self):
         return self.__initialized
 
     async def download_segment(
@@ -42,6 +44,7 @@ class DownloadHLSAsync:
         segment_url: str,
         index: int,
         output_dir: str,
+        progress_bar: IB,
     ) -> None:
         """Асинхронная функция для загрузки одного сегмента."""
 
@@ -52,13 +55,13 @@ class DownloadHLSAsync:
                     with open(segment_path, "wb") as f:
                         content = await response.read()
                         f.write(content)
-                    print(f"Сегмент {index} успешно скачан.")
+                    progress_bar.next()
                 else:
                     logger.error(
-                        f"Ошибка при скачивании сегмента {index}: {response.status}"
+                        f"Error downloading segment {index}: {response.status}"
                     )
         except Exception as ex:
-            logger.error(f"Ошибка при скачивании сегмента {index}: {ex}")
+            logger.error(f"Error downloading segment {index}: {ex}")
 
     async def download_hls_playlist(self) -> None:
         video_path, temp_dir, is_path = self.preparing_dirs(self.path)
@@ -69,22 +72,31 @@ class DownloadHLSAsync:
             return
 
         # Asynchronous loading of all segments
-        async with aiohttp.ClientSession() as session:
-            tasks = list()
-            for i, segment in enumerate(self.segments):
-                segment_url = segment.uri
+        with IB("Downlod chunks:", max=len(self.segments), width=80) as progress_bar:
+            async with aiohttp.ClientSession() as session:
+                tasks = list()
+                for idx, segment in enumerate(self.segments):
+                    segment_url = segment.uri
 
-                # In case the segments contain relative paths, add the base URL
-                if not segment_url.startswith("http"):
-                    segment_url = os.path.join(
-                        os.path.dirname(self.playlist_url),
-                        segment_url,
+                    # In case the segments contain relative paths, add the base URL
+                    if not segment_url.startswith("http"):
+                        segment_url = os.path.join(
+                            os.path.dirname(self.playlist_url),
+                            segment_url,
+                        )
+
+                    tasks.append(
+                        self.download_segment(
+                            session,
+                            segment_url,
+                            idx,
+                            temp_dir,
+                            progress_bar,
+                        )
                     )
 
-                tasks.append(self.download_segment(session, segment_url, i, temp_dir))
-
-            # Launch all tasks simultaneously
-            await asyncio.gather(*tasks)
+                # Launch all tasks simultaneously
+                await asyncio.gather(*tasks)
 
         self.merge_segments(video_path, len(self.segments), temp_dir)
 
@@ -104,16 +116,15 @@ class DownloadHLSAsync:
 
     @staticmethod
     def merge_segments(video_path, len_segments, temp_dir) -> None:
-        # TODO: добавить прогрессбар
-        # Объединение всех сегментов в один файл
-        print("Объединение всех сегментов в один файл")
-        with open(video_path, "wb") as f_out:
-            for i in range(len_segments):
-                segment_path = os.path.join(temp_dir, f"segment_{i}.ts")
-                print(f"{segment_path = }")
-                if os.path.exists(segment_path):
-                    with open(segment_path, "rb") as f_in:
-                        f_out.write(f_in.read())
+        # Merge all segments into a file
+        with IB("Merge segments:", max=len_segments, width=80) as progress_bar:
+            with open(video_path, "wb") as f_out:
+                for i in range(len_segments):
+                    segment_path = os.path.join(temp_dir, f"segment_{i}.ts")
+                    if os.path.exists(segment_path):
+                        with open(segment_path, "rb") as f_in:
+                            f_out.write(f_in.read())
+                            progress_bar.next()
 
         # Deleting temporary segment files along with the folder
         shutil.rmtree(temp_dir)
@@ -123,9 +134,9 @@ class DownloadHLSAsync:
 
 
 if __name__ == "__main__":
-    m3u8_url = "https://playlist.servicecdn1.ru/api/playlist/master/f3ae7682d9952156307f72c6f39cd856/f6de956e62e13fab0311e4e473141544?user-cdn=cdnvideo&acc-id=59148&user-id=400267093&loc-mode=ru&version=17%3A2%3A1%3A0%3A2%3Acdnvideo&consumer=vod&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyLWlkIjo0MDAyNjcwOTN9.CDlwyGyy5kdny9pN3Y4hEN3Y7D_9xvPiElBJvREBUHU"
+    m3u8_url = "https://playlist.servicecdn.ru/api/playlist/master/f3ae7682d9952156307f72c6f39cd856/f6de956e62e13fab0311e4e473141544?user-cdn=cdnvideo&acc-id=59148&user-id=400267093&loc-mode=ru&version=17%3A2%3A1%3A0%3A2%3Acdnvideo&consumer=vod&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyLWlkIjo0MDAyNjcwOTN9.CDlwyGyy5kdny9pN3Y4hEN3Y7D_9xvPiElBJvREBUHU"
     path = "./app/downloads/pages"
 
     download = DownloadHLSAsync(m3u8_url, path)
-    if download.initialized:
+    if download.is_init:
         download.run_async()
